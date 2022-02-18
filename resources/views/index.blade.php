e46c31a56eaab80813dfdb9e4d28c94549d70794
FreshDev <billyhans90@gmail.com>
2022-02-18 19:54:03 +0100

    Add the cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-<h2>Our Content</h2>
+<div class="container-lg" style="margin: 0 auto;">
+    <div class="row mt-5">
+        <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3 mx-5">
+            <div class="card" style="width: 28rem;">
+                <img/>
+                <div class="card-body">
+                    <div class="card-title">
+                        Title
+                    </div>
+                    <div class="card-text">
+                        Content
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
