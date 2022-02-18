17400a8d10be3c464259e8893124e2cc5f602faf
FreshDev <billyhans90@gmail.com>
2022-02-18 20:25:41 +0100

    Update guzzle

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function getData(Request $request) {
-        return view('index');
+        $data="this is my data";
+        return view('index',['data'=>$data]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
                 <img/>
                 <div class="card-body">
                     <div class="card-title">
-                        Title
+                        {{ $data }}
                     </div>
                     <div class="card-text">
                         Content 
